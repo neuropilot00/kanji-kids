1db4a95d6969e29d1f253b7ebdae3356fb3fb84e
neuropilot00 <neuro.pilot00@gmail.com>
2026-05-04 22:26:06 +0900

    Make tablet layout fill viewport

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "kanji-kids-v12";
+const CACHE_NAME = "kanji-kids-v13";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=12",
-  "./kanji-data.js?v=12",
-  "./app.js?v=12",
+  "./styles.css?v=13",
+  "./kanji-data.js?v=13",
+  "./app.js?v=13",
   "./manifest.json"
 ];
 
