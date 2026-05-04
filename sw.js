3994c08971e9e050eec42fcbb05b9008773a67f0
neuropilot00 <neuro.pilot00@gmail.com>
2026-05-04 21:53:02 +0900

    Bust tablet layout cache

--- sw.js
+++ sw.js
@@ -1,10 +1,35 @@
-const CACHE_NAME = "kanji-kids-v7";
-const ASSETS = ["./", "./index.html", "./styles.css", "./kanji-data.js", "./app.js", "./manifest.json"];
+const CACHE_NAME = "kanji-kids-v8";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css?v=8",
+  "./kanji-data.js?v=8",
+  "./app.js?v=8",
+  "./manifest.json"
+];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((names) => Promise.all(names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))))
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
