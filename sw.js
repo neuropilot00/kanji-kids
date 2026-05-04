65451eace4c6d48d29f1df5a63243460a78ced22
neuropilot00 <neuro.pilot00@gmail.com>
2026-05-04 22:16:57 +0900

    Keep tablet kanji card size fixed

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "kanji-kids-v10";
+const CACHE_NAME = "kanji-kids-v11";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=10",
-  "./kanji-data.js?v=10",
-  "./app.js?v=10",
+  "./styles.css?v=11",
+  "./kanji-data.js?v=11",
+  "./app.js?v=11",
   "./manifest.json"
 ];
 
